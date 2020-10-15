b6fe3bcc67e12b0ea254090bce006c273bd5c15b
BarbosOleg <lolipo.celio@gmail.com>
2020-10-15 14:03:56 +0300

    commit

--- header.php
+++ header.php
@@ -1,12 +1,14 @@
 <header>
     <div class="header__row">
         <div class="header__container">
+            <!-- site logo -->
             <div class="header__logo">
                 <a href="#">
                     <img src="./img/logo.png" alt="Logo">
                     <span class="header__name">Kotik</span>
                 </a>
             </div>
+            <!-- block with user information/ registration/ log in -->
             <div class="header__user">
                 <a href="./login.html" class="header__login button">log in</a>
                 <a href="./register.html" class="header__registration button">register</a>
