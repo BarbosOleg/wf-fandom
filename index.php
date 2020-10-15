30bb4f866d7c6ff0b18a04dfa029bd7c83a898b9
BarbosOleg <lolipo.celio@gmail.com>
2020-10-15 13:31:13 +0300

    commit

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="./img/x-logo.png" type="image/x-icon">
+    <title>Warframe Fandom</title>
+</head>
+<body>
+    <div class="wrapper">
+        <!-- header start -->
+        <header>
+            <div class="header__row">
+                <div class="header__container">
+                    <div class="header__logo">
+                        <a href="#">
+                            <img src="./img/logo.png" alt="Logo">
+                            <span class="header__name">Kotik</span>
+                        </a>
+                    </div>
+                    <div class="header__user">
+                        <a href="./login.html" class="header__login button" >log in</a>
+                        <a href="./register.html" class="header__registration button">register</a>
+                    </div>
+                </div>
+
+            </div>
+        </header>
+        <!-- header end -->
+        <!-- content start -->
+        <div class="content__block">
+            <!-- <h1>abracadabra</h1> -->
+            <div class="content__row">
+                <div class="content__banner">
+                    
+                    <div class="content__link">
+                        <ul>
+                            <li><a href="#"><div class="content__li">Main Page</div></a></li>
+                            <li><a href="#"><div class="content__li">About Project</div></a></li>
+                            <li><a href="#"><div class="content__li">Donation</div></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="content__container">
+                    <div class="content__posts">
+                        <!-- Posts start -->
+                        <div class="post__wrapper">
+                            <!-- test content block 1 -->
+                                <div class="content__header">
+                                    <div class="content__user">
+                                        <a href="#"  >
+                                            <div class="content__user__nickname">
+                                                <img src="./img/user_avatar.png" alt="user__avatar" class="user__avatar">
+                                                Vladislav
+                                            </div>                                        
+                                        </a>
+                                    </div>
+                                    <div class="content__title">
+                                        Title1
+                                    </div>
+                                </div>
+                                <div class="content__description">
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad corrupti nostrum similique, sint omnis voluptatem ab deleniti commodi, voluptate vel animi, cumque fugit modi iste enim. Amet, quas. Rerum, obcaecati.</p>
+                                </div>
+                                <div class="content__image">
+                                    <img src="./img/wf--trinity.jpeg" alt="image">
+                                </div>
+                        </div>
+                        <div class="post__wrapper">
+                            <!-- test content block 2 -->
+                                <div class="content__header">
+                                    <div class="content__user">
+                                        <a href="#"  >
+                                            <div class="content__user__nickname">
+                                                <img src="./img/user_avatar.png" alt="user__avatar" class="user__avatar">
+                                                Vladislove
+                                            </div>                                        
+                                        </a>
+                                    </div>
+                                    <div class="content__title">
+                                        Title2
+                                    </div>
+                                </div>
+                                <div class="content__description">
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad corrupti nostrum similique, sint omnis voluptatem ab deleniti commodi, voluptate vel animi, cumque fugit modi iste enim. Amet, quas. Rerum, obcaecati.</p>
+                                </div>
+                                <div class="content__image">
+                                    <img src="./img/wf--wisp.jpeg" alt="image">
+                                </div>
+                        </div>
+                        <!-- Posts end -->
+                        <!-- Pagination start -->
+                        <div class="content__pagination">
+                            <div class="previous_page pagination">
+                                <a href=""><i class="arrow left"></i> Previous</a>
+                            </div>
+                            <div>...</div>
+                            <div class="next__page pagination">
+                                <a href="">Next <i class="arrow right"></i></a>
+                            </div>
+                        </div>
+                        <!-- Pagination start -->
+                    </div>
+                    <div class="content__sidebar">
+                        <div class="popular__posts sidebar__block">
+                            <h3 class="sidebar__heading">Popular posts</h3>
+                            <a href="#">#drop</a>
+                            <a href="#">#farm</a>
+                            <a href="#">#price</a>
+                            <a href="#">#primegarbage</a>
+                            <a href="#">#builds</a>
+                            <a href="#">#fashionframes</a>
+
+                        </div>
+                        <div class="social__media sidebar__block">
+                            <h3 class="sidebar__heading">Find Us</h3>
+                            <a href="https://www.facebook.com" target="_blank"><img src="./img/facebook.png" alt="Facebook" class="social__media__icon"></a>
+                            <a href="https://www.instagram.com" target="_blank"><img src="./img/instagram.png" alt="Instagram" class="social__media__icon"></a>
+                            <a href="https://www.youtube.com" target="_blank"><img src="./img/youtube.png" alt="Youtube" class="social__media__icon"></a>
+                            <a href="https://web.telegram.org" target="_blank"><img src="./img/telegram.png" alt="Telegram" class="social__media__icon"></a>
+                            <a href="https://www.discord.com" target="_blank"><img src="./img/discord.png" alt="Discord" class="social__media__icon"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- content end -->
+        <!-- footer start -->
+        <footer>
+            <div class="footer__row">
+                <div class="upper__footer">
+                    <div class="footer__about">
+                        <ul>
+                            <span class="about">About</span>
+                            <li>Our Story</li>
+                            <li>Site rules</li>
+                            <li>Privacy Policy</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="lower__footer"> 
+                    <div class="footer__copyright">Copyright &copy Barbos Oleg, All right reserved</div>
+                </div>
+
+            </div>
+        </footer>
+        <!-- footer end -->
+    </div>
+</body>
+</html>
